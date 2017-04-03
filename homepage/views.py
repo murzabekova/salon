bf963e71e16a1b990e8e88d35bb93acd3884ec28
murzabekova <amurzabekova01@gmail.com>
2017-04-03 15:30:01 +0600

    updates views

--- homepage/views.py
+++ homepage/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import render
-<<<<<<< HEAD
 from post.models import Post
 import datetime
-=======
 from slider.models import Slider
->>>>>>> 6ce8711f0a3e93560711b73a7a07eb80110c5d37
 
 # Create your views here.
 
